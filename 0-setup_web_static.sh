af2652e9169e4ae6e2ad606cebc43f07510ce991
awandzi <kwamenafo@gmail.com>
2024-05-03 15:56:30 -0700

    zero

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ sudo echo "<html>
         <head>
         </head>
         <body>
-          Holberton School
+		Holberton School
         </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
@@ -25,3 +25,4 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_servers/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
+
